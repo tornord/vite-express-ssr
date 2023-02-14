df7ea16bb0c28a74342e03ba784b415451a88cf6
Tor Nordqvist <tornord@gmail.com>
2023-02-14 09:22:22 +0100

    vite.config

--- packages/marko/vite.config.js
+++ packages/marko/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import marko from "@marko/vite";
 
 export default defineConfig({
-  plugins: [marko({ linked: false })], // marko({ linked: false })
+  plugins: [marko()], // marko({ linked: false })
   build: {
     outDir: "dist",
     // publicDir: "assets",
